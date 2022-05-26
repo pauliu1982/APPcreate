6c902b6a18bfb5a64ded2139ac16b3e3795fe17a
pauliu1982 <user@ST-024>
2022-05-26 15:19:20 +0800

    Practice0526/2022

---
+++ App1/App1/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem1_Clicked(object sender, System.EventArgs e)
+        {
+            Xamarin.Essentials.Browser.OpenAsync("https://jamestsai.tw", 
+                                                  Xamarin.Essentials.BrowserLaunchMode.External);
+        }
+
+        private void MenuItem2_Clicked(object sender, System.EventArgs e)
+        {
+            Xamarin.Essentials.PhoneDialer.Open("1122");
+        }
+    }
+}
---
+++ App1/App1/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)] 
+
+[assembly: Xamarin.Forms.ExportFont("MaterialIcons-Regular.ttf", Alias = "MR")] 
